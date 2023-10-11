679e1b49048f9a7483ed5f653d7fe8a153187157
Dragomy <dragomy1.02@gmail.com>
2023-10-11 14:21:00 +0200

    New Script

---
+++ 3.NewSeperator.py
@@ -0,0 +1,25 @@
+import os
+import json
+import matplotlib.pyplot as plt
+
+folder_path = 'News' 
+
+for filename in os.listdir(folder_path):
+    if filename.endswith('.json'):
+        file_path = os.path.join(folder_path, filename)
+        with open(file_path, 'r') as json_file:
+            try:
+                data = json.load(json_file)
+
+                #Get Value of Tag x
+                for item in data["news"]:
+                  regionId = item.get("regionId")
+                  geotags = item.get("geotags")
+                  
+                #Do something with data:
+                  print(regionId,geotags)
+                
+            except json.JSONDecodeError as e:
+                print(f"Error loading JSON from {filename}: {e}")
+
+
