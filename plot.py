4b49e06adce09055f1f17b65d5798b64a963a08c
Dragomy <95567734+Dragomy@users.noreply.github.com>
2023-11-21 22:04:48 +0100

    updated plot.py

--- plot.py
+++ plot.py
@@ -1,30 +1,33 @@
-from read_data import data_to_dataframe
-from cleaning import clean_all
 import pandas as pd
 
 
-def tag_count(tmp_df = "df"):
-    all_tags = [tag for tags_list in tmp_df['tags'] for tag in tags_list]
-    tag_counts = pd.Series(all_tags).value_counts()
-    return tag_counts
+# Yeah there is no input handling when you are stupid this function breaks
+#Please fix this at some point!!
 
 
-def top_tags(topX):
-    tag_count()
+# If you input a dataframe that has a tag categorie this returns a list of all tags
+def taglist(tmp_df):
+    taglist = [tag for tags_list in tmp_df['tags'] for tag in tags_list]
+    return taglist
+
+
+#This should return a list of all text as well as the number of times the tag exists in the dataset
+def taglist_counted(tmp_df):
+    taglist = [tag for tags_list in tmp_df['tags'] for tag in tags_list]
+    taglist_counted = pd.Series(taglist).value_counts()
+    return taglist_counted
+
+
+def taglist_no_regions(tmp_df):
+    taglist = [tag for tags_list in tmp_df['tags'] for tag in tags_list]
+    taglist_counted = pd.Series(taglist).value_counts()
     
-    top_tags = tag_counts.head(topX)
-    return top_tags
-
-
-def top_tags_without_regions(topX):
-    tags_to_remove = ['Bundesländer','Baden-Württemberg','Bayern','Berlin',
+    to_remove = ['Bundesländer','Baden-Württemberg','Bayern','Berlin',
                       'Brandenburg','Bremen','Hamburg','Hessen',
-                      'Mecklenburg-Vorpommern','Niedersachsen','Nordrhein-Westfalen','Rheinland-Pfalz',
-                      'Saarland','Sachsen','Sachsen-Anhalt','Schleswig-Holstein','Thüringen']
+                      'Mecklenburg-Vorpommern','Niedersachsen','Nordrhein-Westfalen',
+                      'Rheinland-Pfalz','Saarland','Sachsen','Sachsen-Anhalt',
+                      'Schleswig-Holstein','Thüringen']
     
-    tag_count()
-    
-    mask = ~tag_counts.index.isin(tags_to_remove)
-    tags_without_regions = tag_counts[mask]
-    top_tags_without_regions = tags_without_regions.head(topX)
-    return top_tags_without_regions
+    mask = ~taglist_counted.index.isin(to_remove)
+    taglist_no_regions = taglist_counted[mask]
+    return taglist_no_regions
